e6c629bfd030956a755da6672c1cc383486bdc19
Angel <albsabater.bit@gmail.com>
2019-08-06 14:07:25 +0900

    add custom scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
   ** Nuxt.js dev-modules
   */
   devModules: [
-    '@nuxtjs/vuetify',
+    '@nuxtjs/vuetify'
   ],
   /*
   ** Nuxt.js modules
@@ -49,20 +49,7 @@ export default {
   */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    customVariables: ['~/assets/styles.scss'],
   },
   /*
   ** Build configuration
